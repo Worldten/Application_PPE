d1ef414cdaef1db9891da8d51f1a4d2fe7b589e6
corentin-fleurance <corentin.fleurance@epsi.fr>
2019-04-04 14:28:56 +0200

    Git à jour avant oral blanc

--- app/src/main/java/com/example/agence_du_soleil/Filter2.java
+++ app/src/main/java/com/example/agence_du_soleil/Filter2.java
@@ -1,13 +1,37 @@
 package com.example.agence_du_soleil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class Filter2 extends AppCompatActivity {
+
+    float x1, x2, y1, y2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter2);
     }
+
+        /**public boolean onTouchEvent(MotionEvent touchevent){
+            switch (touchevent.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    x1 = touchevent.getX();
+                    y1 = touchevent.getY();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    x2 = touchevent.getX();
+                    y2 = touchevent.getY();
+                    if (x1 < x2) {
+                        Intent i = new Intent(Filter2.this, Filter1.class);
+                        startActivity(i);
+                    }
+                    break;
+            }
+            return false;
+        }**/
+
 }

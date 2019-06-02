bbdf4b388d37b988807e9c88ccacc102a8f3d24e
corentin-fleurance <corentin.fleurance@epsi.fr>
2019-06-02 23:34:07 +0200

    Formulaire ajouté

--- app/src/main/java/com/example/agence_du_soleil/Filters/filtre_swipe.java
+++ app/src/main/java/com/example/agence_du_soleil/Filters/filtre_swipe.java
@@ -5,9 +5,12 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.agence_du_soleil.R;
@@ -19,21 +22,53 @@ public class filtre_swipe extends Activity {
     private EditText ed1, ed2;
     private TextView text;
     private Dialog filtre1;
+    private Dialog filtre2;
     private Dialog filtre3;
+    RadioGroup rg;
+    RadioButton rb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtre_swipe);
         filtre1 = new Dialog(this,android.R.style.Theme_Translucent_NoTitleBar);
+        filtre2 = new Dialog(this,android.R.style.Theme_Translucent_NoTitleBar);
         filtre3 = new Dialog(this,android.R.style.Theme_Translucent_NoTitleBar);
         filtre1.setContentView(R.layout.filtre1_achat);
+        filtre2.setContentView(R.layout.filtre2_achat);
         filtre3.setContentView(R.layout.filtre3_achat);
+        rg = (RadioGroup)  filtre2.findViewById(R.id.radio_recherche);
         addListenerOnButton();
+
 
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(new CustomPagerAdapter(this));
     }
+
+    public void buttonclicked(View view){
+        int radioButtonID = rg.getCheckedRadioButtonId();
+        rb = (RadioButton) filtre2.findViewById(radioButtonID);
+
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (radioButtonID){
+            case R.id.radio_piece1:
+                if (checked)
+                    break;
+            case R.id.radio_piece2:
+                if (checked)
+                    break;
+            case R.id.radio_piece3:
+                if (checked)
+                    break;
+            case R.id.radio_piece4:
+                if (checked)
+                    break;
+            case R.id.radio_piece5etplus:
+                if (checked)
+                    break;
+        }
+    }
+
 
     public void addListenerOnButton() {
         appartement = (CheckBox) findViewById(R.id.checkb_appart);
